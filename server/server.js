f49f9c78961675d6374d417f359b65f8babb489d
KhushiAgg3404 <aggarwalkhushi2025@gmail.com>
2026-06-16 00:20:21 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -96,8 +96,8 @@ Return ONLY valid JSON.
     }
 });
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
