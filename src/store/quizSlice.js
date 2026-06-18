69e5a642234cee479229866c14cb7d8ace3eb6e8
KhushiAgg3404 <aggarwalkhushi2025@gmail.com>
2026-06-18 21:10:56 +0530

    Update quizSlice.js

--- src/store/quizSlice.js
+++ src/store/quizSlice.js
@@ -1,118 +1,147 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    questions: [],
-    currentQuestionIndex: 0,
-    answers: [],
-    isQuizCompleted: false,
-    score: 0,
-    timeLeft: 300,
-    isTimerActive: false,
-    showExplanation: false,
+  questions: [],
+  currentQuestionIndex: 0,
+  answers: [],
+  isQuizCompleted: false,
+  score: 0,
+
+  // Timer
+  timeLeft: 300,
+  timer: 300,
+
+  isTimerActive: false,
+  showExplanation: false,
 };
 
 const quizSlice = createSlice({
-    name: "quiz",
-    initialState,
-    reducers: {
-        setQuestions: (state, action) => {
-            state.questions = action.payload;
-        },
+  name: "quiz",
+  initialState,
 
-        startQuiz: (state) => {
-            state.currentQuestionIndex = 0;
-            state.answers = [];
-            state.isQuizCompleted = false;
-            state.score = 0;
-            state.isTimerActive = true;
-            state.showExplanation = false;
-        },
-
-        answerQuestions: (state, action) => {
-            const { questionId, selectedOption } = action.payload;
-
-            const question = state.questions.find((q) => q.id === questionId);
-            const isCorrect = selectedOption === question.correctAnswer;
-
-            const existingIndex = state.answers.findIndex(ans => ans.questionId === questionId);
-
-            const answer = {
-                questionId,
-                selectedOption,
-                isCorrect
-            };
-
-            if (existingIndex !== -1) {
-                // Replace existing answer
-                state.answers[existingIndex] = answer;
-            } else {
-                // Add new answer
-                state.answers.push(answer);
-            }
-
-            // Recalculate score
-            state.score = state.answers.filter((a) => a.isCorrect).length;
-
-            // Always show explanation after answering
-            state.showExplanation = true;
-        },
-
-
-        decrementTimer: (state) => {
-            if (state.timeLeft > 0 && state.isTimerActive) {
-                state.timeLeft -= 1;
-            } else if (state.timeLeft === 0) {
-                state.isQuizCompleted = true;
-                state.isTimerActive = false;
-            }
-        },
-        setTimer: (state, action) => {
-            state.timeLeft = action.payload;
-        },
-
-
-        nextQuestion: (state) => {
-            if (state.currentQuestionIndex < state.questions.length - 1) {
-                state.currentQuestionIndex += 1;
-                const currentId = state.questions[state.currentQuestionIndex].id;
-                const answered = state.answers.find((a) => a.questionId === currentId);
-                state.showExplanation = !!answered;
-            } else {
-                state.isQuizCompleted = true;
-                state.isTimerActive = false;
-            }
-        },
-
-        previousQuestion: (state) => {
-            if (state.currentQuestionIndex > 0) {
-                state.currentQuestionIndex -= 1;
-                const currentId = state.questions[state.currentQuestionIndex].id;
-                const answered = state.answers.find((a) => a.questionId === currentId);
-                state.showExplanation = !!answered;
-            }
-        },
-
-        resetQuiz: (state) => {
-            state.currentQuestionsIndex = 0;
-            state.answers = [];
-            state.isQuizCompleted = false;
-            state.score = 0;
-            state.timeLeft = 300;
-            state.isTimerActive = false;
-            state.showExplanation = false;
-        }
+  reducers: {
+    setQuestions: (state, action) => {
+      state.questions = action.payload;
     },
+
+    startQuiz: (state) => {
+      state.currentQuestionIndex = 0;
+      state.answers = [];
+      state.isQuizCompleted = false;
+      state.score = 0;
+      state.isTimerActive = true;
+      state.showExplanation = false;
+    },
+
+    answerQuestions: (state, action) => {
+      const { questionId, selectedOption } = action.payload;
+
+      const question = state.questions.find(
+        (q) => q.id === questionId
+      );
+
+      const isCorrect =
+        selectedOption === question.correctAnswer;
+
+      const existingIndex = state.answers.findIndex(
+        (ans) => ans.questionId === questionId
+      );
+
+      const answer = {
+        questionId,
+        selectedOption,
+        isCorrect,
+      };
+
+      if (existingIndex !== -1) {
+        state.answers[existingIndex] = answer;
+      } else {
+        state.answers.push(answer);
+      }
+
+      state.score = state.answers.filter(
+        (a) => a.isCorrect
+      ).length;
+
+      state.showExplanation = true;
+    },
+
+    decrementTimer: (state) => {
+      if (state.timeLeft > 0 && state.isTimerActive) {
+        state.timeLeft -= 1;
+      } else if (state.timeLeft === 0) {
+        state.isQuizCompleted = true;
+        state.isTimerActive = false;
+      }
+    },
+
+    setTimer: (state, action) => {
+      state.timeLeft = action.payload;
+      state.timer = action.payload;
+    },
+
+    nextQuestion: (state) => {
+      if (
+        state.currentQuestionIndex <
+        state.questions.length - 1
+      ) {
+        state.currentQuestionIndex += 1;
+
+        const currentId =
+          state.questions[state.currentQuestionIndex].id;
+
+        const answered = state.answers.find(
+          (a) => a.questionId === currentId
+        );
+
+        state.showExplanation = !!answered;
+      } else {
+        state.isQuizCompleted = true;
+        state.isTimerActive = false;
+      }
+    },
+
+    previousQuestion: (state) => {
+      if (state.currentQuestionIndex > 0) {
+        state.currentQuestionIndex -= 1;
+
+        const currentId =
+          state.questions[state.currentQuestionIndex].id;
+
+        const answered = state.answers.find(
+          (a) => a.questionId === currentId
+        );
+
+        state.showExplanation = !!answered;
+      }
+    },
+
+    resetQuiz: (state) => {
+      state.currentQuestionIndex = 0;
+      state.answers = [];
+      state.isQuizCompleted = false;
+      state.score = 0;
+
+      state.timeLeft = 300;
+      state.timer = 300;
+
+      state.isTimerActive = false;
+      state.showExplanation = false;
+
+      state.questions = [];
+    },
+  },
 });
 
 export const {
-    setQuestions,
-    setTimer,
-    startQuiz,
-    decrementTimer,
-    answerQuestions,
-    nextQuestion,
-    previousQuestion,
-    resetQuiz,
+  setQuestions,
+  setTimer,
+  startQuiz,
+  decrementTimer,
+  answerQuestions,
+  nextQuestion,
+  previousQuestion,
+  resetQuiz,
 } = quizSlice.actions;
 
 export default quizSlice.reducer;
